d7cb5f8f4e87bc1dcf785b9238ffd9eac1feeb73
Alvin Hardian <alvin.hardian@gmail.com>
2024-02-07 20:42:14 +0700

    iims

--- hello.php
+++ hello.php
@@ -1,33 +1,49 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
+use Mike42\Escpos\PrintConnectors\NetworkPrintConnector;
 use Mike42\Escpos\Printer;
 use Mike42\Escpos\CapabilityProfile;
 $profile = CapabilityProfile::load("POS-5890");
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['name']) && isset($_POST['code']) && isset($_POST['kanwil'])) {
-        $name = $_POST['name'];
-        $code = $_POST['code'];
-        $kanwil = $_POST['kanwil'];
+    if (isset($_POST['customer_qrcode'])) {
+        $name = $_POST['customer_name'];
+		$id = $_POST['id'];
+		$id = sprintf("%04d", $id);
+		$cname = $_POST['customer_childname'];
+        $code = $_POST['customer_qrcode'];
         $response = array('status' => 'success', 'message' => 'Data received successfully');
-		$connector = new WindowsPrintConnector("POS-80");
+		$connector = new NetworkPrintConnector("192.168.0.31",9100);
+
+		//$connector = new WindowsPrintConnector("POS-80");
 		$printer = new Printer($connector);
 		$printer -> initialize();
+		// PARENT SHEET
 		$printer -> setJustification(Printer::JUSTIFY_CENTER);
 		$printer -> selectPrintMode(Printer::MODE_DOUBLE_HEIGHT | Printer::MODE_DOUBLE_WIDTH);
-		$printer -> text("46 Tahun BP Jamsostek\n");
-		$printer -> text("Employee Gathering\n\n");
+		$printer -> text("Kidzania X Danamon\n\n");
 		$printer -> selectPrintMode(Printer::MODE_FONT_B);
 		$printer -> setTextSize(2, 2);
+		$printer -> text($id."\n");
+		$printer -> text("ORANG TUA :\n");
 		$printer -> text($name."\n");
-		$printer -> text("KANWIL " . $kanwil."\n");
 		$printer -> feed();
 		$printer -> qrCode($code, Printer::QR_ECLEVEL_H, 8, Printer::QR_MODEL_1);
 		$printer -> feed();
+		$printer -> cut();
+		//CHILD SHEET
+		$printer -> setJustification(Printer::JUSTIFY_CENTER);
+		$printer -> selectPrintMode(Printer::MODE_DOUBLE_HEIGHT | Printer::MODE_DOUBLE_WIDTH);
+		$printer -> text("Kidzania X Danamon\n\n");
+		$printer -> selectPrintMode(Printer::MODE_FONT_B);
+		$printer -> setTextSize(2, 2);
+		$printer -> text($id."\n");
+		$printer -> text("ANAK :\n");
+		$printer -> text($cname."\n");
 		$printer -> feed();
+		$printer -> qrCode($code, Printer::QR_ECLEVEL_H, 8, Printer::QR_MODEL_1);
 		$printer -> feed();
 		$printer -> cut();
 		$printer -> close();
